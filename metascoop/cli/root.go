66d8a44cd40057e26c9de9d7b25eb7049f5b8bda
Pedro Mangabeira <pedro.mangabeira@sky.uk>
2024-05-14 05:18:48 +0100

    meta

--- metascoop/cli/root.go
+++ metascoop/cli/root.go
@@ -140,7 +140,7 @@ func (g *Globals) updateAndPull() error {
 			setNonEmpty(meta, "Name", fn)
 			setNonEmpty(meta, "SourceCode", apkInfo.GitURL)
 			setNonEmpty(meta, "License", apkInfo.License)
-			setNonEmpty(meta, "Website", apkInfo.Website)
+			setNonEmpty(meta, "WebSite", apkInfo.Website)
 			setNonEmpty(meta, "IssueTracker", apkInfo.IssueTracker)
 			setNonEmpty(meta, "Description", apkInfo.Description)
 
